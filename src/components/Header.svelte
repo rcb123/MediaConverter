0695c13c2a50bceaadd4a585a8356201930f9133
Reza Banankhah <banankha@usc.edu>
2024-12-28 01:48:22 -0800

    Fix button element
    Previously button was being stretched due to flex-grow
    Now container for button is stretched, and button remains normal width

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -12,16 +12,18 @@
 			<Button variant="link" href="/gallery">Gallery</Button>
 		</div>
 
-		<Button onclick={toggleMode} variant="ghost" size="icon" class="grow basis-0 justify-end">
-			<Sun
-				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all 
+		<div class="flex grow basis-0 justify-end">
+			<Button onclick={toggleMode} variant="ghost" size="icon">
+				<Sun
+					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all 
           dark:-rotate-90 dark:scale-0"
-			/>
-			<Moon
-				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 
+				/>
+				<Moon
+					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 
           transition-all dark:rotate-0 dark:scale-100"
-			/>
-			<span class="sr-only">Toggle theme</span>
-		</Button>
+				/>
+				<span class="sr-only">Toggle theme</span>
+			</Button>
+		</div>
 	</div>
 </header>
